bda8558e75716d19f06ff9e30675a18a83fdec59
Antas raj pathak <sijjupathaksijju@gmail.com>
2025-04-23 13:09:44 +0530

    projects link

--- portfolio/src/component/Projects.jsx
+++ portfolio/src/component/Projects.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "../styles/Projects.css";
+import web from '../assets/webAnimations.png'
+import currency from '../assets/currency.png'
+import yt from '../assets/youtube.png'
+import Antas from '../assets/Antas.png'
+import BM from '../assets/Bookmanagement.png'
+
+
+
 
 const Projects = () => {
   return (
@@ -8,20 +16,20 @@ const Projects = () => {
         <div className="project-left">
           <div className="project-left-image-logo">
             <img
-              src="https://cdn.pixabay.com/photo/2021/09/11/12/17/github-6615451_1280.png"
+              src="https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png"
               alt=""
             />
           </div>
           <div className="project-left-content">
-            <h3>Book Management App</h3>
-            <p className="project-meta">
+            <h3>Advance UI </h3>
+            <a href="https://web-animations-seven.vercel.app/" className="project-meta">
               <button className="view-project-btn">View Project</button>
-            </p>
+            </a>
           </div>
         </div>
         <div className="project-right">
           <img
-            src="https://cdn.pixabay.com/photo/2017/03/23/12/23/vintage-2168174_1280.jpg"
+            src={web}
             alt=""
           />
         </div>
@@ -37,66 +45,15 @@ const Projects = () => {
             />
           </div>
           <div className="project-left-content">
-            <h3>Book Management App</h3>
-            <p className="project-meta">
+            <h3>Youtube(MERN Stack)</h3>
+            <a href="https://github.com/Antasraj12get/YouTube.git" className="project-meta">
               <button className="view-project-btn">View Project</button>
-            </p>
+            </a>
           </div>
         </div>
         <div className="project-right">
           <img
-            src="https://cdn.pixabay.com/photo/2017/03/23/12/23/vintage-2168174_1280.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-
-
-
-      <div className="project">
-        <div className="project-left">
-          <div className="project-left-image-logo">
-            <img
-              src="https://cdn.pixabay.com/photo/2021/09/11/12/17/github-6615451_1280.png"
-              alt=""
-            />
-          </div>
-          <div className="project-left-content">
-            <h3>Book Management App</h3>
-            <p className="project-meta">
-              <button className="view-project-btn">View Project</button>
-            </p>
-          </div>
-        </div>
-        <div className="project-right">
-          <img
-            src="https://cdn.pixabay.com/photo/2017/03/23/12/23/vintage-2168174_1280.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-
-
-
-
-      <div className="project">
-        <div className="project-left">
-          <div className="project-left-image-logo">
-            <img
-              src="https://cdn.pixabay.com/photo/2021/09/11/12/17/github-6615451_1280.png"
-              alt=""
-            />
-          </div>
-          <div className="project-left-content">
-            <h3>Book Management App</h3>
-            <p className="project-meta">
-              <button className="view-project-btn">View Project</button>
-            </p>
-          </div>
-        </div>
-        <div className="project-right">
-          <img
-            src="https://cdn.pixabay.com/photo/2017/03/23/12/23/vintage-2168174_1280.jpg"
+            src={yt}
             alt=""
           />
         </div>
@@ -113,15 +70,91 @@ const Projects = () => {
             />
           </div>
           <div className="project-left-content">
-            <h3>Book Management App</h3>
-            <p className="project-meta">
+            <h3>ShoppyGlobe Backend ( E Commerce)</h3>
+            <a href="https://github.com/Antasraj12get/Backend-shoppyGlobe.git" className="project-meta">
               <button className="view-project-btn">View Project</button>
-            </p>
+            </a>
           </div>
         </div>
         <div className="project-right">
           <img
-            src="https://cdn.pixabay.com/photo/2017/03/23/12/23/vintage-2168174_1280.jpg"
+            src="https://d3m1rm8xuevz4q.cloudfront.net/wp-content/uploads/2022/10/Model-View-Controller.png.webp"
+            alt=""
+          />
+        </div>
+      </div>
+
+
+
+      <div className="project">
+        <div className="project-left">
+          <div className="project-left-image-logo">
+            <img
+              src="https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png"
+              alt=""
+            />
+          </div>
+          <div className="project-left-content">
+            <h3>Portfolio</h3>
+            <a href="https://antas-raj-pathak-new.vercel.app/" className="project-meta">
+              <button className="view-project-btn">View Project</button>
+            </a>
+          </div>
+        </div>
+        <div className="project-right">
+          <img
+            src={Antas}
+            alt=""
+          />
+        </div>
+      </div>
+
+
+
+
+      <div className="project">
+        <div className="project-left">
+          <div className="project-left-image-logo">
+            <img
+              src="https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png"
+              alt=""
+            />
+          </div>
+          <div className="project-left-content">
+            <h3>Book Management App</h3>
+            <a href="https://books-management-application.vercel.app/" className="project-meta">
+              <button className="view-project-btn">View Project</button>
+            </a>
+          </div>
+        </div>
+        <div className="project-right">
+          <img
+            src={BM}
+            alt=""
+          />
+        </div>
+      </div>
+
+
+
+      <div className="project">
+        <div className="project-left">
+          <div className="project-left-image-logo">
+            <img
+              src="https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png"
+              alt=""
+            />
+          </div>
+          <div className="project-left-content">
+            <h3>Currency Converter</h3>
+            <a href="https://currency-converter-three-inky.vercel.app/" className="project-meta">
+              <button className="view-project-btn">View Project</button>
+            </a>
+          </div>
+        </div>
+        <div className="project-right">
+          <img
+            src={currency}
             alt=""
           />
         </div>
